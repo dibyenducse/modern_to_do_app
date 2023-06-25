86e7cb2fef2148aefb5bba6f29c1ec2ff0422632
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-06-26 01:11:32 +0600

    new bug

--- routehandler/todoHandler.js
+++ routehandler/todoHandler.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const todoSchema = express;
+const todoSchema = require('../schemas/todoSchema');
+const mongoose = require('mongoose');
+const Todo = mongoose.model('Todo', todoSchema);
 //Get all the todos
 router.get('/', async (req, res) => {});
 
 //Get a todo by id
 router.get('/:id', async (req, res) => {});
 
-//post todo
-router.post('/', async (req, res) => {});
+//post a todo
+router.post('/', async (req, res) => {
+    const newTodo = new Todo(req.body);
+    await newTodo.save();
+});
 
 //post multiple todo
 router.post('/', async (req, res) => {});
--- schemas/todoSchema.js
+++ schemas/todoSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const todoSchema = mongoose.Schema({
-    titlel: {
+const todoSchema = new mongoose.Schema({
+    title: {
         type: String,
         required: true,
     },
