7ebfa7842bf489c80c0d9093c4a63a6d4ea2aad9
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-06-25 23:36:03 +0600

    connection with mongo successfully

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 
 //express initialization
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 //application routes
 
-
+//database connection with mongoose
+mongoose
+    .connect('mongodb://localhost/mordern_to_do_app', {})
+    .then(() => {
+        console.log('connection sucessfull');
+    })
+    .catch((err) => console.log(err));
 
 //default error handler
 function errorHandler(err, req, res, next) {
     if (res.headersent) {
-        return next (err)
+        return next(err);
     }
-    res.status(500).json({ error: err })
+    res.status(500).json({ error: err });
 }
+
+app.listen(3000, () => {
+    console.log('app listening at port 3000');
+});
