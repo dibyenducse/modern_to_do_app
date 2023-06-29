a7b6ea96263d9dcd88400abae29034d384332f40
Dibyendu Dibbo <dibyendu.cse.ru@gmail.com>
2023-06-30 01:59:32 +0600

    bug fixed by using try and catch

--- routehandler/todoHandler.js
+++ routehandler/todoHandler.js
@@ -1,27 +1,49 @@
 const express = require('express');
 const todoSchema = require('../schemas/todoSchema');
 const mongoose = require('mongoose');
-const Todo = new mongoose.model('Todo', todoSchema); //mongoose= Elegant Object Data Modeling
+const Todo = mongoose.model('Todo', todoSchema); //mongoose= Elegant Object Data Modeling
 
 const router = express.Router();
 //Get all the todos
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+    await Todo.find({ status: 'active' }, (err) => {
+        if (err) {
+            res.status(500).json({
+                error: 'There was an error',
+            });
+        } else {
+            res.status(200).json({
+                message: 'Todo inserted successfully',
+            });
+        }
+    });
+});
 
 //Get a todo by id
 router.get('/:id', async (req, res) => {});
 
 //post a todo
 router.post('/', async (req, res) => {
-    const newTodo = new Todo(req.body);
-    await newTodo.save();
-    res.send('Data Listed on Database successfully');
+    try {
+        const newTodo = Todo(req.body);
+        await newTodo.save();
+        res.send('Data Listed on Database successfully');
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
 });
 
 //post multiple todo
 router.post('/all', async (req, res) => {
-    await Todo.insertMany(req.body);
-    console.log(Todo);
-    res.send('Data Listed on Database successfully');
+    try {
+        await Todo.insertMany(req.body);
+        console.log(Todo);
+        res.send('Data Listed on Database successfully');
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
 });
 
 //put todo update todo
